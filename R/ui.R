fdd9774abd486c7379641057c0f120157274771e
Alex Prieto <123606666+AlexPrietoRomani@users.noreply.github.com>
2025-11-08 11:07:27 -0500

    Refactor course selection UX

--- R/ui.R
+++ R/ui.R
@@ -1,28 +1,26 @@
 # R/ui.R
 library(shiny)
 library(shinythemes)
+library(bslib)
 
 ui <- fluidPage(
   withMathJax(),
-  theme = bs_theme(bootswatch = "flatly"),
+  theme = bs_theme(
+    version = 5,
+    bootswatch = "flatly",
+    primary = "#2F855A",
+    secondary = "#2B6CB0",
+    success = "#38A169",
+    base_font = font_google("Poppins"),
+    heading_font = font_google("Montserrat"),
+    bg = "#F7FAF2",
+    fg = "#1F2933"
+  ),
   tags$head(
+    tags$link(rel = "preconnect", href = "https://fonts.googleapis.com"),
+    tags$link(rel = "preconnect", href = "https://fonts.gstatic.com", crossorigin = "anonymous"),
     includeCSS("www/css/custom.css"),
     includeScript("www/js/custom.js")
   ),
-
-  titlePanel("📊 Temario de R para Estadística Agrícola"),
-
-  sidebarLayout(
-    sidebarPanel(
-      width = 2,
-      h4("Navegación"),
-      selectInput("curso", "Curso:", choices = names(estructura_cursos), selected = curso_predeterminado),
-      radioButtons("parte", "Parte:", choices = names(estructura_cursos[[curso_predeterminado]]), selected = partes_predeterminadas[1]),
-      uiOutput("sesion_ui")
-    ),
-    mainPanel(
-      width = 10,
-      uiOutput("contenido_ui")
-    )
-  )
+  uiOutput("main_ui")
 )
