ffbd14c31f2800697cab3a1b836ec10b245925ca
Alex Prieto <alexprieto1997@gmail.com>
2025-11-09 06:12:12 -0500

    add gitignore

--- intermedio.R
+++ intermedio.R
@@ -6,11 +6,12 @@ setwd("C:/Users/ALEX/OneDrive/Asesorias/Escuela CEDEPA/2da capacitación/Curso_d
 
 # Vector con todas las librerías requeridas
 pkgs <- c(
-  "shiny", "tidyverse", "agricolae", "readxl", "janitor",
-  "moments", "plotly", "bslib", "ggplot2", "patchwork",
-  "grid", "car", "dplyr", "MASS", "effectsize", "broom",
-  "emmeans", "DT", "pwr", "dunn.test", "rstatix",
-  "ggfortify", "scatterplot3d", "GGally", "effects"
+  "shiny", "bslib", "shinythemes",
+  "tidyverse", "readxl", "janitor", "broom", "moments", "systemfonts",
+  "agricolae", "MASS", "emmeans", "effectsize", "pwr", "dunn.test", "rstatix", "lme4", "lmerTest",
+  "ggplot2", "plotly", "patchwork", "GGally", "ggfortify", "scatterplot3d", "effects", "RColorBrewer", "grid",
+  "DT", "car",
+  "FielDHub", "augmentedRCBD", "broom.mixed"
 )
 
 # Instalar sólo las que no estén instaladas
